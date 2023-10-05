7f657f18c6c8108952e08a6fff66a6fcdc48e545
Kshivashankar12 <kshivashankargowda1999@gmail.com>
2023-10-05 14:59:11 +0530

    Add project files.

---
+++ Tempforgithub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tempforgithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helloworld");
+        }
+    }
+}
